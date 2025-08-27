e7f03eda5802b2bf0873466d20303eeb0b0d0e03
Asbendra8707 <ashwinmahakal123@gmail.com>
2025-08-27 13:29:50 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const env = require ("dotenv")
 env.config()//Load envirnment variable
-
+const cron=require('node-cron')
+const axios=require('axios')
 const express =require('express')
 const app= express() //Initialize express
 const path=require('path')
@@ -11,7 +12,21 @@ const { connectDB } = require('./database/dbConnection')
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'dist/index.html'))
 })
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+  });
+
 app.use('/api', userRouter);
+
+cron.schedule('*/10 * * * *', async () => {
+    try {
+      const res = await axios.get('https://asbendra-todo-app.onrender.com/health');
+      console.log(`Self-ping success: ${res.status}`);
+    } catch (err) {
+      console.error('Self-ping failed:', err.message);
+    }
+  });
 
 const PORT = process.env.PORT || 5000;
 
